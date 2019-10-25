377fc366166c4a66fb163c2224905ac185506779
Rudy Crespo <crespor@montclair.edu>
2019-10-25 16:24:12 -0400

    removed unused var

--- src/components/DecisionTree.js
+++ src/components/DecisionTree.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useState, useReducer } from "react";
 import { Transition } from "react-transition-group";
 
 import nodesData from "../nodes.json";
